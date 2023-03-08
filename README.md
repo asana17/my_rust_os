122ac453a697e48dddb90bdbfe006c38e490f299
asana17 <kei17.kaight084@gmail.com>
2023-03-08 22:07:42 +0900

    add iretq to naked interrupt

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 ## my_rust_os
 This OS is based on [blog_os](https://os.phil-opp.com).
+
+## objdump
+
+```
+objdump -m i386:x86-64 -b binary -D target/x86_64-blog_os/debug/bootimage-blog_os.b
+in > objdump.txt
+```
+offset: 0x1f2e00
+if exception at 0x203ef7
+look at 110f7
--- src/naked_interrupts/mod.rs
+++ src/naked_interrupts/mod.rs
@@ -4,15 +4,28 @@ use crate::println;
 use core::arch::asm;
 use lazy_static::lazy_static;
 
+
 macro_rules! handler {
     ($name:ident) => {{
         #[naked]
         extern "C" fn wrapper() -> ! {
             unsafe {
-                asm!("mov rdi, rsp",
-                     "sub rsp, 8", //align the stack pointer
-                    concat!("call ", stringify!($name)), // call handler_function
-                    options(noreturn),
+                asm!("push rax",
+                     "push rcx",
+                     "push rdx",
+                     "push rsi",
+                     "push rdi",
+                     "push r8",
+                     "push r9",
+                     "push r10",
+                     "push r11", //save caller-saved registers by callee to handle interrupt
+                     "mov rdi, rsp",
+                     "add rdi, 9*8", //calculate exception stack frame pointer
+                     //"sub rsp, 8", //align the stack pointer, this time stack is aligned already
+                     concat!("call ", stringify!($name)), // call handler_function
+                     //"add rsp, 8", //undo stack pointer alignment, not needed anymore
+                     "iretq",
+                     options(noreturn),
                 )
             }
         }
@@ -25,10 +38,32 @@ macro_rules! handler_with_error_code {
         #[naked]
         extern "C" fn wrapper() -> ! {
             unsafe {
-                asm!("pop rsi", //pop error code into rsi
+                asm!("push rax",
+                     "push rcx",
+                     "push rdx",
+                     "push rsi",
+                     "push rdi",
+                     "push r8",
+                     "push r9",
+                     "push r10",
+                     "push r11", //save caller-saved registers by callee to handle interrupt
+                     "mov rsi, [rsp+9*8]",//load error code into rsi, not pop
                      "mov rdi, rsp",
-                     "sub rsp, 8",
+                     "add rdi, 10*8", //calculate exception stack frame pointer
+                     "sub rsp, 8", //align the stack pointer
                      concat!("call ", stringify!($name)),
+                     "add rsp, 8", //undo stack pointer alignment
+                     "pop r11",
+                     "pop r10",
+                     "pop r9",
+                     "pop r8",
+                     "pop rdi",
+                     "pop rsi",
+                     "pop rdx",
+                     "pop rcx",
+                     "pop rax", //restore caller-saved register by callee to handle interrupt
+                     "add rsp, 8", //increment stack pointer instead of popping error code
+                     "iretq",
                      options(noreturn),
                 )
             }
@@ -38,11 +73,12 @@ macro_rules! handler_with_error_code {
 }
 
 
+
 lazy_static!{
     static ref IDT: idt::Idt = {
         let mut idt = idt::Idt::new();
-        //idt.set_handler(0, divide_by_zero_wrapper);
         idt.set_handler(0, handler!(divide_by_zero_handler));
+        idt.set_handler(3, handler!(breakpoint_handler));
         idt.set_handler(6, handler!(invalid_opcode_handler));
         idt.set_handler(14, handler_with_error_code!(page_fault_handler));
         idt
@@ -50,26 +86,24 @@ lazy_static!{
 }
 
 #[no_mangle]
-extern "C" fn divide_by_zero_handler(stack_frame: &ExceptionStackFrame) -> ! {
+extern "C" fn divide_by_zero_handler(stack_frame: &ExceptionStackFrame) {
 
-    println!("\nEXCEPTION: DIVIDE BY ZERO\n{:#?}", stack_frame);
+    println!("\nEXCEPTION: DIVIDE BY ZERO at {:#x}\n{:#?}", stack_frame.instruction_pointer, stack_frame);
 
     loop {}
 }
 
 #[no_mangle]
-extern "C" fn invalid_opcode_handler(stack_frame: &ExceptionStackFrame) -> ! {
+extern "C" fn invalid_opcode_handler(stack_frame: &ExceptionStackFrame) {
     println!("\nEXCEPTION: INVALID OPCODE at {:#x}\n{:#?}", stack_frame.instruction_pointer, stack_frame);
 
     loop {}
 }
 
 #[no_mangle]
-extern "C" fn page_fault_handler(stack_frame: &ExceptionStackFrame, error_code:u64) -> ! {
+extern "C" fn page_fault_handler(stack_frame: &ExceptionStackFrame, error_code:u64) {
     use x86_64::registers::control;
     println!("\nEXCEPTION: PAGE FAULT while accessing {:#x}\nerror code: {:?}\n{:#?}", control::Cr2::read(), PageFaultErrorCode::from_bits(error_code).unwrap(), stack_frame);
-
-    loop {};
 }
 
 bitflags! {
@@ -80,6 +114,12 @@ bitflags! {
         const MALFORMED_TABLE = 1 << 3;
         const INSTRUCTION_FETCH = 1 << 4;
     }
+}
+
+#[no_mangle]
+extern "C" fn breakpoint_handler(stack_frame: &ExceptionStackFrame) {
+    println!("\nEXCEPTION: BREAKPOINT at {:#x}\n{:#?}",
+             stack_frame.instruction_pointer, stack_frame);
 }
 
 
